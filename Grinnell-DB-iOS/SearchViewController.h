fbe63022a26933ea5eca3ef01ec86a0aa3b0123a
Colin Tremblay <tremblay@grinnell.edu>
2013-01-28 09:45:33 -0600

    Starting the TableView Version

---
+++ Grinnell-DB-iOS/SearchViewController.h
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.h
+//  Grinnell-DB-iOS
+//
+//  Created by Colin Tremblay on 1/28/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@property (nonatomic, strong) NSMutableArray *fieldsArray;
+@end
