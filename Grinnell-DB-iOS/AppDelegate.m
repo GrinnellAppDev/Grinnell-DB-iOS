adb552a1e0b72c18182cf0a1fa143ec3b8f862ed
Alex Mitchell <alex@alexthemitchell.com>
2016-02-21 13:26:30 -0600

    Increment build number; remove Fabric cruft

--- Grinnell-DB-iOS/AppDelegate.m
+++ Grinnell-DB-iOS/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  Grinnell-DB-iOS
-//
-//  Created by AppDev on 9/11/13.
-//  Copyright (c) 2013 AppDev. All rights reserved.
-//
-
 #import "AppDelegate.h"
 #import "FormViewController.h"
 #import <Crashlytics/Crashlytics.h>
@@ -14,10 +6,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    //NSString *strings_private = [[NSBundle mainBundle] pathForResource:@"strings_private" ofType:@"strings"];
-    //NSDictionary *keysDict = [NSDictionary dictionaryWithContentsOfFile:strings_private];
     [Fabric with:@[[Crashlytics self]]];
-    //[Crashlytics startWithAPIKey:[keysDict objectForKey:@"CrashlyticsAPIKey"]];
     
     return YES;
 }
@@ -32,23 +21,6 @@
     [navC popToRootViewControllerAnimated:NO];
     FormViewController *formVC = (FormViewController *)[navC.childViewControllers objectAtIndex:0];
     [formVC clear:self];
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
