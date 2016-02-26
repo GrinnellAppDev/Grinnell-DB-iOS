9226ede401c9e27f18db6d4b16b0e939e67683b6
Alex Mitchell <alex@alexthemitchell.com>
2016-02-26 17:44:42 -0600

    Convert AppDelegate to Swift

--- Grinnell-DB-iOS/Grinnell-DB-iOS-Bridging-Header.h
+++ Grinnell-DB-iOS/Grinnell-DB-iOS-Bridging-Header.h
@@ -2,5 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "FormViewController.h"
 #import "ResultsViewController.h"
 #import "ProfileViewController.h"
