76a75671d85b7a0630104739edd66173bce7075c
Alex Mitchell <alex@alexthemitchell.com>
2016-02-26 17:44:42 -0600

    Convert AppDelegate to Swift

---
+++ Grinnell-DB-iOS/AppDelegate.swift
@@ -0,0 +1,25 @@
+import Fabric
+import Crashlytics
+import UIKit
+
+public class AppDelegate: UIResponder, UIApplicationDelegate {
+  public var window: UIWindow?
+
+  public func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    Fabric.with([Crashlytics.self])
+    return true
+  }
+
+  public func applicationWillResignActive(application: UIApplication) {
+    reset(application)
+  }
+
+  func reset(application: UIApplication) {
+    NSNotificationCenter.defaultCenter().postNotificationName("DismissImage", object: nil)
+    let window = application.delegate?.window!
+    let navigationVC = window!.rootViewController as! UINavigationController
+    navigationVC.popToRootViewControllerAnimated(false)
+    let form = navigationVC.childViewControllers.first as! FormViewController
+    form.clear(self)
+  }
+}
